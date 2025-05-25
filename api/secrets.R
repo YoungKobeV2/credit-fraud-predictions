3d36b31868342a7568c3b69d4eeff371ae98da7f
Lebintiti Kobe <153471364+YoungKobeV2@users.noreply.github.com>
2025-05-25 15:02:33 +0200

    Update secrets.R

--- api/secrets.R
+++ api/secrets.R
@@ -1,3 +1,6 @@
+# This file should secret because it contains yout API keys . 
+
+# Created fake login details , use this to access the API predict endpoints 
 Sys.setenv("ADMIN1"="Jason Keller:JK12345")
 Sys.setenv("ADMIN2"="Mariam Williams:MW12345")
 Sys.setenv("ADMIN3"="Young Kobe:YK12345")
